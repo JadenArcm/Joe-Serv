3c890f94c4dda8cd59f379c68389e03af789f106
JadenArcm <jadenarcalt@gmail.com>
2023-01-28 12:21:22 -0500

    more tips.....,.,.,.

--- source/Lua/Core/Visual/HUD/Miscellaneous/tips.lua
+++ source/Lua/Core/Visual/HUD/Miscellaneous/tips.lua
@@ -6,13 +6,18 @@ local CVAR_COLOR = "\x87"
 local tipTexts = {
 	"You can type " .. CMD_COLOR .. "colorize" .. "\x80 on your console to become, well, colorized!",
 	"No rings? Get some with " .. CMD_COLOR .. "setrings" .. "\x80!",
+	{"Wanna be super for some reason? Use " .. CMD_COLOR .. "togglesuper" .. "\x80!", "(You also need some \x83" .. "emeralds" .. "\x80...)"},
 	{"You died? Got 1 life? No problem! " .. CMD_COLOR .. "setlives" .. "\x80 is your solution.", "(You won't be reborn if you \x85" .. "Game Over" .. "\x80'd.)"},
 
 	{"Be respectful to others, please.", "If doing otherwise, you will be either \x85" .. "muted" .. "\x80, \x85" .. "kicked" .. "\x80 or \x85" .. "banned" .. "\x80."},
 
 	"You can use commands on the chat! Just use the \x86" .. "/<command>" .. "\x80 prefix, and call it a day.",
+	{"Wanna search some \x83" .. "emblems" .. "\x80?", "Toggle " .. CVAR_COLOR .. "joe_emblemradar" .. "\x80 and go for all of those emblems!"},
 	{"You don't really like names on top of every player? Disable it with " .. CVAR_COLOR .. "joe_nametags" .. "\x80.",  "(and " .. CVAR_COLOR .. "joe_maxtags" .. "\x80 to see a few according to your preference.)"},
 	{"You can see how the bosses are!", "With " .. CVAR_COLOR .. "joe_bosstags" .. "\x80, you can see the amount of health they have."},
+
+	"\x85" .. "Joe" .. "\x80 is watching you everywhere. No matter what. ALL HAIL \x85" .. "JOE" .. "\x80!",
+	"\x85" .. "among us" .. "\x80. are you the \x85" .. "among us" .. "\x80? all hail \x85" .. "among us" .. "\x80.",
 }
 
 --//
@@ -28,7 +33,7 @@ local function handleTickers()
 
 	if not (joeVars.tipTimer) then
 		joeVars.tipDelay = 6 * TICRATE
-		joeVars.tipTimer = 180 * TICRATE
+		joeVars.tipTimer = 7 * TICRATE
 
 		joeVars.tipText = tipTexts[P_RandomRange(1, #tipTexts)]
 		S_StartSound(nil, sfx_jtip, nil)
